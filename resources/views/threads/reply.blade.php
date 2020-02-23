abe2a10d860f03f72322f34f9a6361bafd5aa10d
Chee <cheevt@gmail.com>
2020-02-23 19:43:08 +0100

    add like button to make a favorite reply

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,7 +1,15 @@
 <div class="card">
-  <div class="card-header">
-    <a href="#">{{ $reply->user->name }}</a> - 
-    {{ $reply->created_at->diffForHumans() }}
+  <div class="card-header article-header">
+    <div class="article-header--title">
+      <a href="#">{{ $reply->user->name }}</a> - 
+      {{ $reply->created_at->diffForHumans() }}
+    </div>
+    <div>
+      <form action="{{ route('favorites.store', ['reply' => $reply]) }}" method="POST">
+        @csrf
+        <button type="submit" {{ !$reply->favorites()->exists() ?: 'disabled' }}>{{ $reply->favorites()->count() }} {{ str_plural('Like', $reply->favorites()->count()) }}</button>
+      </form>
+    </div>
   </div>
   <div class="card-body">
       {{ $reply->body }}
