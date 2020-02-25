fdf1c6583413424b8b95f734689b7a15ca1d1b24
Chee <cheevt@gmail.com>
2020-02-25 19:43:37 +0100

    add my profile link to dropdown menu, reorganize index of threads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,74 +31,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('threads.index') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item"><a href="{{ route('threads.index') }}" class="nav-link">All Threads</a></li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Boards
-                            </a>
-
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                @foreach ($boards as $board)
-                                    <a href="/threads/{{ $board->slug }}" class="dropdown-item">{{ $board->name }}</a>
-                                @endforeach
-                            </div>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="{{ route('threads.index') }}?popular=1" class="nav-link">Popular</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a href="{{ route('threads.create') }}" class="dropdown-item">Create a new thread</a>
-                                    <a href="{{ route('threads.index') }}?by={{ auth()->user()->name }}" class="dropdown-item">My threads</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('layouts.partials._nav')
 
         <main class="py-4">
             @yield('content')
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,29 +4,28 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Forum Threads</div>
+            <div class="card-header">Forum Threads</div>
+            
+                @foreach ($threads as $thread)
+                    <div class="card mb-3">
+                        <div class="card-header">
+                                <div class="article-header">
+                                    <h4 class="article-header--title">
+                                        <a href="{{ $thread->show_url() }}">{{ $thread->title }}</a>
+                                    </h4>
 
-                <div class="card-body">
-                    @foreach ($threads as $thread)
-                        <article class="pt-2 py-2">
-                            <div class="article-header">
-                                <h4 class="article-header--title">
-                                    <a href="{{ $thread->show_url() }}">{{ $thread->title }}</a>
-                                </h4>
+                                    <a href="{{ $thread->show_url() }}">
+                                        {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
+                                    </a>
+                                </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="body">{{ $thread->body }}</div>
 
-                                <a href="{{ $thread->show_url() }}">
-                                    {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
-                                </a>
-                            </div>
-                          
-                          <div class="body">{{ $thread->body }}</div>
-                        </article>
-
-                        <hr>
-                    @endforeach
-                </div>
-            </div>
+                            <hr>
+                        </div>
+                    </div>
+                @endforeach
         </div>
     </div>
 </div>
